c7bd9fc2e5eb8cf2e6d238ea93c26195d425b8b5
“Ivan <ivan.madera1999@gmail.com>
2022-03-01 17:23:46 -0600

    JS Documentation already done

---
+++ src/.vuepress/config.js
@@ -0,0 +1,57 @@
+module.exports = {
+  dest: 'docs',
+  base: '/js-documentation/',
+  title: 'Javascript Docs',
+  description: 'javascript documentation',
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+  theme: 'yuu',
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: 'Github',
+        link: 'https://github.com/IvanMadera',
+      },
+    ],
+    sidebar: {
+      '/js-guide/': [
+        {
+          title: 'Guide',
+          collapsable: false,
+          children: [
+            '',
+            '01',
+            '02',
+            '03',
+            '04',
+            '05',
+            '06',
+            '07',
+            '08',
+            '09',
+            '10',
+            '11',
+            '12',
+            '13',
+          ]
+        }
+      ],
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
